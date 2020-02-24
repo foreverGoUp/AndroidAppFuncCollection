a3e7e432ec2addb8a6e64a65f7d645e44f569ca2
peter <successfulpeter@163.com>
2020-02-25 03:16:41 +0800

    1、新增环形能量控件,水平滚动缩放相册控件

--- module_home/src/main/java/com/kc/module_home/widget/SmallWidgetShowActivity.java
+++ module_home/src/main/java/com/kc/module_home/widget/SmallWidgetShowActivity.java
@@ -1,27 +1,124 @@
 package com.kc.module_home.widget;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
 import com.dovar.router_annotation.Route;
+import com.kc.common_service.base.AppBaseActivity;
 import com.kc.module_home.R;
+import com.kc.module_home.databinding.ActivitySmallWidgetShowBinding;
+import com.kc.module_home.widget.horizontalScrollScaleGallery.HorizontalScrollScaleGalleryHelper;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ckc.android.develophelp.lib.base.mvvm.BaseViewModel;
 import ckc.android.develophelp.lib.util.ToastUtils;
 import ckc.android.develophelp.lib.widget.SquareVerifyCodeView;
 
 @Route(path = "widget/small")
-public class SmallWidgetShowActivity extends AppCompatActivity {
+public class SmallWidgetShowActivity extends AppBaseActivity<ActivitySmallWidgetShowBinding, BaseViewModel> {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_small_widget_show);
-        SquareVerifyCodeView verifyCodeView = findViewById(R.id.square);
-        verifyCodeView.setOnVerifyCodeListener(new SquareVerifyCodeView.OnVerifyCodeListener() {
+    protected int onConfigContentViewLayout() {
+        return R.layout.activity_small_widget_show;
+    }
+
+    @Override
+    protected void onInit(Bundle savedInstanceState) {
+        mDataBinding.squareVerifyCodeView.setOnVerifyCodeListener(new SquareVerifyCodeView.OnVerifyCodeListener() {
             @Override
             public void onVerifyCodeInputComplete(final String code) {
                 ToastUtils.showToast(SmallWidgetShowActivity.this, code);
             }
         });
+
+
+        mDataBinding.bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String input = mDataBinding.et1.getText().toString();
+                if (TextUtils.isEmpty(input)) {
+                    return;
+                }
+                int num = Integer.parseInt(input);
+                if (num >= 0 && num <= 100) {
+                    mDataBinding.annularEnergyView.setCurrentEnergyWithAnimation(num);
+                }
+            }
+        });
+
+
+        mDataBinding.rv1.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        List<String> data = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            data.add(String.valueOf(i));
+        }
+        final HorizontalScrollScaleGalleryHelper helper = new HorizontalScrollScaleGalleryHelper(mDataBinding.rv1, 8, 50, 30, 0.6f);
+        final HorizontalScrollScaleGalleryAdapter adapter = new HorizontalScrollScaleGalleryAdapter(helper, data);
+        mDataBinding.rv1.setAdapter(adapter);
+
+        mDataBinding.bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String input = mDataBinding.et2.getText().toString();
+                if (TextUtils.isEmpty(input)) {
+                    return;
+                }
+                int num = Integer.parseInt(input);
+                helper.scrollToPosition(num);
+            }
+        });
+
+        mDataBinding.bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToastUtils.showToast(SmallWidgetShowActivity.this, "当前位置：" + adapter.mHelper.getCurrentItemPos());
+            }
+        });
+
+    }
+
+    @Override
+    protected void onConfigViewModelAndBindDataBinding() {
+
+    }
+
+    private class HorizontalScrollScaleGalleryAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
+
+        HorizontalScrollScaleGalleryHelper mHelper;
+
+        public HorizontalScrollScaleGalleryAdapter(HorizontalScrollScaleGalleryHelper helper, @Nullable List<String> data) {
+            super(R.layout.item_horizontal_scroll_scale_gallery, data);
+            mHelper = helper;
+        }
+
+        @NonNull
+        @Override
+        public BaseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            BaseViewHolder baseViewHolder = super.onCreateViewHolder(parent, viewType);
+            mHelper.onCreateViewHolder(parent, baseViewHolder.itemView, baseViewHolder.getLayoutPosition());
+            return baseViewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(BaseViewHolder holder, int position) {
+            super.onBindViewHolder(holder, position);
+            mHelper.onBindViewHolder(holder.itemView, position, getItemCount());
+        }
+
+
+        @Override
+        protected void convert(@NonNull BaseViewHolder helper, String item) {
+            helper.setText(R.id.tv_content, item);
+        }
     }
 }
